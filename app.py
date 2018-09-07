f397f3d6428bdc790c5e57d31a1bf1d3fcf746dd
sw1227 <32032108+sw1227@users.noreply.github.com>
2018-09-07 21:11:58 +0900

    add Road Direction

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, jsonify
+import requests
+import json
 
 # ---------- Variables ----------
 app = Flask(__name__)
@@ -10,10 +11,37 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+
 @app.route("/ca")
 def ca():
+    """ 1D Cellular Automaton """
     return render_template("ca.html")
+
 
 @app.route("/lifegame")
 def lifegame():
+    """ Conway's Game of Life """
     return render_template("lifegame.html")
+
+
+@app.route("/road_direction")
+def road_direction():
+    """ Road direction histogram """
+    return render_template("road_direction.html")
+
+
+@app.route("/road_geojson")
+def road_geojson():
+    """ Get GeoJSON of road """
+    endpoint = "https://cyberjapandata.gsi.go.jp/xyz/experimental_rdcl/16/{x}/{y}.geojson"
+    response = requests.get(endpoint.format(
+        x = request.args.get("x"),
+        y = request.args.get("y")
+    ))
+    return jsonify(json.loads(response.text))
+
+
+@app.route("/lsystem")
+def lsystem():
+    """ L-system """
+    return render_template("lsystem.html")
