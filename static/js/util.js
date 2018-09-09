e940f1e9cd9cdde4882c7a7f56b36045e0c1f31d
sw1227 <32032108+sw1227@users.noreply.github.com>
2018-09-09 14:51:08 +0900

    add dropdowns to complex

--- static/js/util.js
+++ static/js/util.js
@@ -3,8 +3,8 @@ function imshow(elementId, array, size, interpolate=d3.interpolateViridis) {
     var parent = d3.select(`#${elementId}`)
 	.attr("class", "card")
 	.attr("style", `width: ${size}px; height: ${size}px;`);
-    var canvas = parent.append("canvas")
-	.attr("class", "card-img");
+    var canvas = parent.select("canvas")
+    canvas = canvas.empty() ? parent.append("canvas").attr("class", "card-img") : canvas;
 
     var resolution = Math.sqrt(array.length);
     canvas.attr("width", resolution).attr("height", resolution);
@@ -20,4 +20,52 @@ function imshow(elementId, array, size, interpolate=d3.interpolateViridis) {
         imageData.data[i*4+3] = 255;
     });
     context.putImageData(imageData, 0, 0);
+}
+
+
+// Generate Dropdown given each item's text and callback
+function generateDropdown(data, parentId, name) {
+    var dropdown = d3.select(`#${parentId}`).append("div")
+        .attr("class", "dropdown");
+
+    dropdown.append("button")
+        .attr("class", "btn btn-primary dropdown-toggle")
+        .attr("id", `dropdown-${name}`)
+        .attr("type", "button")
+	.attr("data-toggle", "dropdown")
+        .attr("aria-haspopup", "true")
+	.attr("aria-expanded", "false")
+        .text(name);
+
+    dropdown.append("div")
+        .attr("class", "dropdown-menu")
+        .attr("aria-labelledby", `dropdown-${name}`)
+      .selectAll("a").data(data).enter().append("a")
+        .attr("class", "dropdown-item")
+        .attr("onclick", d => d.callback)
+        .text(d => d.text);
+}
+
+
+// Calculate proximity to lattice
+function latticeProximity(x, y, alpha=0.01) {
+    // alpha: prevent divergence of log - the smaller the sharper
+    var dx = - Math.log(alpha + Math.abs(x - Math.round(x)));
+    var dy = - Math.log(alpha + Math.abs(y - Math.round(y)));
+    // Normalize
+    const minVal = - 2 * Math.log(alpha + 0.5);
+    const maxVal = - 2 * Math.log(alpha);
+    return (dx + dy - minVal) / (maxVal - minVal); // range [0, 1]
+}
+
+
+// (x, y) => 0 or 1
+function checkerBoard(x, y) {
+    return Math.abs(Math.abs(Math.floor(x) % 2) - Math.abs(Math.floor(y) % 2))
+}
+
+
+// (x, y) => Argument(angle) [-Pi, +PI] => [0, 2*PI] => [0, 1]
+function normalizedArg(x, y) {
+    return (math.atan2(y, x) + 2*Math.PI) % (2*Math.PI) / (2*Math.PI);
 }
