3b5dac86f155e6817755c66ee98f919c807f9516
sw1227 <32032108+sw1227@users.noreply.github.com>
2018-10-06 21:43:27 +0900

    use HashRouter

--- react/src/App.js
+++ react/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import StyledDrawer from './Drawer';
 
 
