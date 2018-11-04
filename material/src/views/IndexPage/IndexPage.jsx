2c0035ea3878ffbd8c03b5362f975cf6911a6a15
sw1227 <32032108+sw1227@users.noreply.github.com>
2018-11-04 22:45:47 +0900

    add Links to index for material

--- material/src/views/IndexPage/IndexPage.jsx
+++ material/src/views/IndexPage/IndexPage.jsx
@@ -1,15 +1,37 @@
 import React from "react";
 import withStyles from "@material-ui/core/styles/withStyles";
-
+import { Link } from "react-router-dom";
 import Frame from "../common/Frame.jsx";
+
+const indexRoutes = [
+    { path: "/harmonograph", name: "Harmonograph" },
+    { path: "/inversion", name: "Inversion" },
+    { path: "/calabiyau", name: "CalabiYau" },
+    { path: "/riemann", name: "Riemann" },
+    { path: "/hata", name: "Hata" },
+    { path: "/complex", name: "Complex" },
+    { path: "/ca", name: "Ca" },
+    { path: "/lifegame", name: "GameOfLife" },
+    { path: "/lsystem", name: "Lsystem" },
+    { path: "/quad", name: "Quad" },
+    { path: "/road", name: "RoadDirection" },
+];
 
 class IndexPage extends React.Component {
     render() {
         const { classes } = this.props;
         return (
             <Frame title="Visualizations">
-                <p>Hello, world!</p>
                 {/* TODO: List of categories */}
+                <ul>
+                    {indexRoutes.map(route => (
+                        <li>
+                            <Link to={route.path}>
+                                {route.name}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
             </Frame>
         );
     }
